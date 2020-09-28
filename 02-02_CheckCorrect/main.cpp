901caf1478d7b48566a4a689157c88468d8131cf
Мызников Никита <shadow4571@mail.ru>
2020-09-28 11:32:13 +0500

    02-02_CheckCorrect - <2. Функция проверки корректности скобок в строке>

---
+++ 02-02_CheckCorrect/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool CheckBracketExpression(string Expression) {
+    vector<char> BRACKETS_OPEN = vector<char>({'(', '{', '[', '<'});
+    vector<char> BRACKETS_CLOSE = vector<char>({')', '}', ']', '>'});
+
+    stack<char> LastOpen = stack<char>();
+
+    for(int i = 0; i < Expression.size(); i++) {
+        if(find(BRACKETS_OPEN.begin(), BRACKETS_OPEN.end(), Expression[i]) != BRACKETS_OPEN.end()) {
+            LastOpen.push(Expression[i]);
+        }
+
+        if(!LastOpen.empty()) {
+            vector<char>::iterator Symbol = find(BRACKETS_CLOSE.begin(), BRACKETS_CLOSE.end(), Expression[i]);
+            
+            if(Symbol != BRACKETS_CLOSE.end()) {
+                if(LastOpen.top() == BRACKETS_OPEN[distance(BRACKETS_CLOSE.begin(), Symbol)]) {
+                    LastOpen.pop();
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            if(find(BRACKETS_CLOSE.begin(), BRACKETS_CLOSE.end(), Expression[i]) != BRACKETS_CLOSE.end())
+                return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    string Expression = string(")");
+
+    cout << boolalpha << "Result: " << CheckBracketExpression(Expression);
+
+    return 0;
+}
